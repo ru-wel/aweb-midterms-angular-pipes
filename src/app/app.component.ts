24b875f519dff83a7ae134687f0c1079be6a0fea
kumibriones2 <dumpkumi21@gmail.com>
2025-01-15 10:29:40 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { interval } from 'rxjs';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-pipes-demo';
+  presentDate = new Date();
+  time$ = interval(1000).pipe(
+    map(() => new Date()) 
+  )
+  testObject = {
+    name: 'Reuel Christian Sundiam',
+    age: 25,
+    food: 'Cheesecake',
+    numbers: [1, 2, 3, 4, 5]
+  }
+  testArray = [1, 2, 3, 4, 5];
+  price : number = 20000;
+  Fruits = ["Apple", "Orange", "Grapes", "Mango", "Kiwi", "Pomegranate"]; ngOnInit(){
+
+  }
+  str: string = 'abcdefghij';
+  decimalNum1: number = 8.7589623;
+  decimalNum2: number = 5.43;
+  pi: number = 3.14159265359;
 }
